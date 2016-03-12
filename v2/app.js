ad6a1a2e69181b71798bc98de08a66d3b869bfbe
Randy Torres <torresr14@gmail.com>
2016-03-11 22:38:44 -0800

    Added comments schema and modules

--- v2/app.js
+++ v2/app.js
@@ -1,72 +1,70 @@
 var express    = require("express"),
     app        = express(),
     bodyParser = require("body-parser"),
-    mongoose   = require("mongoose");
+    mongoose   = require("mongoose"),
+    Campground = require('./models/campgrounds'),
+    seedDB     = require('./seeds');
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+
+mongoose.connect("mongodb://localhost/yelp_camp/v2");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String
-});
-
-//4:31 of video
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-  Campground.create(
-    {
-
-      name : "Salmon Creek",
-      image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRjj0VfmU-j0DDIyl7Hgi-F3VnU3_NSCRYPgSjIjPwADixTpmp6Cw"
-
-    }, function(err, campground) {
-         if(err) {
-           console.log(err);
-         } else {
-           console.log("Newly Created Campground")
-           console.log(campground)
-         }
-
-    });
+seedDB();
 
 app.get("/", function(req, res){
     res.render("landing");
     console.log("someone is checking out the landing page");
 });
 
+
 app.get("/campgrounds", function(req, res){
   Campground.find({}, function(err, allCampgrounds) {
     if (err) {
       console.log(err)
     } else {
-      res.render("campgrounds", {campgrounds:allCampgrounds});
+      res.render("index", {campgrounds:allCampgrounds});
     }
   })
 });
 
-app.post("/campgrounds", function(req, res){
-    // get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    // Crate a new campground andsave to database
-    Campground.create(newCampground, function(err, newlyCreated) {
-      if (err) {
-        console.log(err)
-      } else {
-        res.redirect("/campgrounds");
-      }
-    });
-  });
 
-app.get("/campgrounds/new", function(req, res){
-    res.render("new.ejs");
+app.post("/campgrounds", function(req, res){
+  // get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
+  // Crate a new campground andsave to database
+  Campground.create(newCampground, function(err, newlyCreated) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new.ejs");
+});
+
+
+// Show - shows more info about each campground
+app.get("/campgrounds/:id", function(req, res) {
+  // Find campground with provided ID
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log(foundCampground);
+      res.render("show", {campground: foundCampground});
+    }
+  });
+})
+
+
 app.listen(3000, process.env.IP, function(){
-    console.log("YelpCamp has started");
+  console.log("YelpCamp has started");
 });
 
---
+++ v2/seeds.js
@@ -0,0 +1,65 @@
+var mongoose = require('mongoose');
+var Campground = require('./models/campgrounds');
+var Comment = require('./models/comment');
+
+var data = [
+
+  { name: "Clouds Rest",
+	image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjKssHZuLrLAhVC2mMKHWjfBxsQjRwIBw&url=http%3A%2F%2Fwww.fs.usda.gov%2Factivity%2Fipnf%2Frecreation%2Fcamping-cabins%2F%3Frecid%3D6762%26actid%3D29&psig=AFQjCNHJptMzejhUHvt9yrYrOTYaU2U-xw&ust=1457847867492034',
+  	description: "Yadayadayada"
+  },
+  { name: "Clouds Rest",
+	image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjKssHZuLrLAhVC2mMKHWjfBxsQjRwIBw&url=http%3A%2F%2Fwww.fs.usda.gov%2Factivity%2Fipnf%2Frecreation%2Fcamping-cabins%2F%3Frecid%3D6762%26actid%3D29&psig=AFQjCNHJptMzejhUHvt9yrYrOTYaU2U-xw&ust=1457847867492034',
+  	description: "Yadayadayada"
+  },
+  { name: "Clouds Rest",
+	image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjKssHZuLrLAhVC2mMKHWjfBxsQjRwIBw&url=http%3A%2F%2Fwww.fs.usda.gov%2Factivity%2Fipnf%2Frecreation%2Fcamping-cabins%2F%3Frecid%3D6762%26actid%3D29&psig=AFQjCNHJptMzejhUHvt9yrYrOTYaU2U-xw&ust=1457847867492034',
+  	description: "Yadayadayada"
+  },
+  { name: "Clouds Rest",
+	image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjKssHZuLrLAhVC2mMKHWjfBxsQjRwIBw&url=http%3A%2F%2Fwww.fs.usda.gov%2Factivity%2Fipnf%2Frecreation%2Fcamping-cabins%2F%3Frecid%3D6762%26actid%3D29&psig=AFQjCNHJptMzejhUHvt9yrYrOTYaU2U-xw&ust=1457847867492034',
+  	description: "Yadayadayada"
+  }
+
+];
+
+function seedDB() {
+  // Remove all campgrounds
+  Campground.remove({}, function (err) {
+	if (err) {
+	  console.log(err)
+	}
+
+	console.log("removed campgrounds");
+	// add a few campgrounds
+	data.forEach(function(seed) {
+	  Campground.create(seed, function(err, campground) {
+		if(err) {
+		  console.log(err)
+		} else {
+		  console.log("added a campground");
+		  // create a comment on each campground
+		  Comment.create(
+			{
+			  text: "This is place is great!",
+			  author: "Homer"
+			}, function(err, comment) {
+			  if (err) {
+				console.log(err)
+			  } else {
+				campground.comments.push(comment);
+				campground.save();
+				console.log("Created a new comment")
+			  }
+			}
+		  )
+		}
+	  })
+	})
+  });
+
+
+
+}
+
+module.exports = seedDB;
